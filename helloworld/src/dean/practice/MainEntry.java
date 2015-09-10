e8d2ef3b5a92e607743b0673f602d1d73507e83b
Dean <Dean@Dean-VAIO>
2015-09-10 20:25:28 +0800

    2nd version

--- helloworld/src/dean/practice/MainEntry.java
+++ helloworld/src/dean/practice/MainEntry.java
@@ -5,6 +5,7 @@ public class MainEntry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloworld");
+		System.out.println("2nd version");
 
 	}
 
