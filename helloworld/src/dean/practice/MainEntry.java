4a17497a30ff3fd1619594463507b0a8d0d29674
Dean <Dean@Dean-VAIO>
2015-09-10 23:10:50 +0800

    6th version done

--- helloworld/src/dean/practice/MainEntry.java
+++ helloworld/src/dean/practice/MainEntry.java
@@ -4,12 +4,13 @@ public class MainEntry {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloworld");
+		System.out.println("helloworld, hotfix done");
 		System.out.println("2nd version");
 		System.out.println("3rd version");
 		System.out.println("4th version");
 		System.out.println("5th version");
 		System.out.println("6th version half done");
+		System.out.println("6th version done");
 
 	}
 
