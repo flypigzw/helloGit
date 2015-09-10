70e863a3159771f1cd82c36962db2403946d6124
Dean <Dean@Dean-VAIO>
2015-09-10 21:35:54 +0800

    3rd version

--- helloworld/src/dean/practice/MainEntry.java
+++ helloworld/src/dean/practice/MainEntry.java
@@ -6,6 +6,7 @@ public class MainEntry {
 		// TODO Auto-generated method stub
 		System.out.println("helloworld");
 		System.out.println("2nd version");
+		System.out.println("3rd version");
 
 	}
 
