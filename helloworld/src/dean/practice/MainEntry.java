5262149cf8e0fd1da1caeae9bb08d6621a4d4522
Dean <Dean@Dean-VAIO>
2015-09-10 20:23:20 +0800

    Init

---
+++ helloworld/src/dean/practice/MainEntry.java
@@ -0,0 +1,11 @@
+package dean.practice;
+
+public class MainEntry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+
+	}
+
+}
